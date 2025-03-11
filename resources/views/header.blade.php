1ea6a36d5b24c30ea6d58436c60c8d9bc0a3e4c5
TimotyDarrelDavidson <c14230235@john.petra.ac.id>
2025-03-11 17:58:49 +0700

    improving homepage body, and seperating header to be something included

---
+++ resources/views/header.blade.php
@@ -0,0 +1,13 @@
+<div class="w-full bg-gray-900 ">
+    <header class="lg:px-16 px-4 flex flex-wrap items-center py-4 shadow-lg">
+        <div class="hidden md:flex md:items-center md:w-auto w-full" id="menu">
+            <nav>
+                <ul class="md:flex items-center justify-between text-base text-white pt-4 md:pt-0">
+                    <li><a class="md:p-4 py-3 px-0 block text-rose-500" href="/">Home</a></li>
+                    <li><a class="md:p-4 py-3 px-0 block" href="katalog">Katalog</a></li>
+                    <li><a class="md:p-4 py-3 px-0 block md:mb-0 mb-2" href="#">About Us</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,33 +6,36 @@
     <title>Toko Bintang</title>
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 </head>
-<body class="h-screen">
-    <div class="w-full bg-gray-900 ">
-        <header class="lg:px-16 px-4 flex flex-wrap items-center py-4 shadow-lg">
-            <div class="hidden md:flex md:items-center md:w-auto w-full" id="menu">
-                <nav>
-                    <ul class="md:flex items-center justify-between text-base text-white pt-4 md:pt-0">
-                        <li><a class="md:p-4 py-3 px-0 block text-rose-500" href="katalog">Home</a></li>
-                        <li><a class="md:p-4 py-3 px-0 block" href="#">Katalog</a></li>
-                        <li><a class="md:p-4 py-3 px-0 block md:mb-0 mb-2" href="#">About Us</a></li>
-                    </ul>
-                </nav>
+<body>
+    @include('header')
+
+    <div class="relative isolate overflow-hidden w-full min-h-[calc(100vh-5.5rem)] bg-cover bg-center bg-no-repeat py-24 sm:py-32">
+        <img src="{{ asset('images/homepage_image.jpg') }}" 
+            alt="" 
+            class="absolute inset-0 -z-10 w-full h-full object-cover">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8 bg-gray-900/50">
+            <div class="mx-auto max-w-2xl lg:mx-0">
+                <h2 class="text-5xl font-semibold tracking-tight text-white sm:text-7xl">Welcome to Toko Tiga Bingtang</h2>
+                <p class="mt-8 text-lg font-medium text-pretty sm:text-xl/8 text-gray-200">Kita menjual berbagai macam perhiasan unik dan emas dari yang ringan 0,1gr hingga berat 100gr</p>
             </div>
-        </header>
-    </div>
-    <div class="w-full h-full bg-gray-500">
-        <div class="w-full mx-auto pt-20 relative xl:px-16 px-8 flex md:flex-row flex-col gap-4 justify-center items-center pb-10">
-            <div class="flex flex-col justify-center gap-4 md:text-left text-center text-white md:mt-0 sm:mt-8 mt-4">
-                <h1 class="md:text-7xl sm:text-3xl text-2xl font-semibold font-serif">Welcome to Toko Bintang</h1>
-                <p class="md:text-2xl">Jual emas dalam berbagai jenis dan berat</p>
-                <div class="sm:mt-4 mt-2">
-                    <button class="px-6 py-2 bg-rose-500 text-white rounded-sm">Get In Touch</button>
-                </div>
+        
+            <div class="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+                <dl class="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
+                    <div class="flex flex-col-reverse gap-1">
+                        <dt class="text-base/7 text-gray-300">open since</dt>
+                        <dd class="text-4xl font-semibold tracking-tight text-white">2004</dd>
+                    </div>
+                    <div class="flex flex-col-reverse gap-1">
+                        <dt class="text-base/7 text-gray-300">shop open time</dt>
+                        <dd class="text-4xl font-semibold tracking-tight text-white">11:00 - 17:00</dd>
+                    </div>
+                    <div class="flex flex-col-reverse gap-1">
+                        <dt class="text-base/7 text-gray-300">Location</dt>
+                        <dd class="text-4xl font-semibold tracking-tight text-white">Pasar Atom Mall</dd>
+                    </div>
+                </dl>
             </div>
         </div>
     </div>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.12/typed.min.js" referrerpolicy="no-referrer"></script>
-</script>
 </body>
 </html>
